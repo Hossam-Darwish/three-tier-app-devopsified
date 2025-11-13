23723009cb12061307faf8fac3b424d2dce7226b
hossam-darwish <116852461+Hossam-Darwish@users.noreply.github.com>
2025-11-13 10:27:43 -0500

    terraform -code-correction

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,28 +1,30 @@
 module "eks" {
   source          = "terraform-aws-modules/eks/aws"
-  version         = "~> 21.0"
-  name    = local.cluster_name
-  kubernetes_version = var.kubernetes_version
+  version         = "20.8.4"
+  cluster_name    = local.cluster_name
+  cluster_version = var.kubernetes_version
   subnet_ids      = module.vpc.private_subnets
 
-  enable_cluster_creator_admin_permissions = true
   enable_irsa = true
 
   tags = {
-    cluster = "demo"
+    cluster = "practice"
   }
 
   vpc_id = module.vpc.vpc_id
 
+  eks_managed_node_group_defaults = {
+    ami_type               = "AL2_x86_64"
+    instance_types         = ["t3.small"]
+    vpc_security_group_ids = [aws_security_group.all_worker_mgmt.id]
+  }
+
   eks_managed_node_groups = {
-    example = {
-      ami_type               = "AL2_x86_64"
-      instance_types         = ["t3.small"]
-      
+
+    node_group = {
       min_size     = 2
       max_size     = 4
       desired_size = 2
     }
   }
-
 }
